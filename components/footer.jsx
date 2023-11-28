0c936ce859b248389bb425fcad9f086a1ea77cf8
0xABDee <113326190+Dawood67@users.noreply.github.com>
2023-11-28 17:58:14 +0500

    footer updated

--- components/footer.jsx
+++ components/footer.jsx
@@ -9,23 +9,28 @@ import uniswap from "@/public/images/uniswap.png";
 import Image from "next/image";
 const Footer = () => {
   return (
-    <div className="flex flex-col lg:flex-row justify-between items-center pb-4">
-      <div>
-        <Image src={logo} />
+    <>
+      <div className="flex flex-col lg:flex-row justify-between items-center pb-4">
+        <div>
+          <Image src={logo} />
+        </div>
+        <div>
+          <h1 className=" text-[#1E1F25] text-center max-lg:text-[14px]  break-all lg:ml-[10rem]">
+            CA: 0xA187927C9185108458647AEEC193ef4A62d3bd80
+          </h1>
+        </div>
+        <div className="flex justify-end gap-7 max-lg:mt-4  ">
+          <Image src={uniswap} className="w-[1.88331rem] h-[2.15231rem]" />
+          <Image src={twitter} className="w-[2.0815rem] h-[1.93794rem]" />
+          <Image src={dextool} className="w-[1.88688rem] h-[1.88688rem]" />
+          <Image src={telegram} className="w-[2.07919rem] h-[1.97344rem]" />
+          <Image src={etherscan} className="w-[1.88688rem] h-[1.88688rem]" />
+        </div>
       </div>
-      <div>
-        <h1 className=" text-[#1E1F25] text-center  max-lg:text-[14px]  break-all">
-          CA: 0xA187927C9185108458647AEEC193ef4A62d3bd80
-        </h1>
-      </div>
-      <div className="flex justify-end gap-8 max-lg:mt-4 object-contain">
-        <Image src={twitter} />
-        <Image src={telegram} />
-        <Image src={etherscan} />
-        <Image src={dextool} />
-        <Image src={uniswap} />
-      </div>
-    </div>
+      <p className="text-[#1E1F25] text-[0.9375rem] font-normal italic text-center pb-[1rem]">
+        © 2023 - All Rights Reserved
+      </p>
+    </>
   );
 };
 
