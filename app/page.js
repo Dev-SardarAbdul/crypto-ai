6dac2c4aecf5dd4eae0e72beca270c4248c9e06b
0xABDee <113326190+Dawood67@users.noreply.github.com>
2023-11-28 17:21:57 +0500

    arti development added

--- app/page.js
+++ app/page.js
@@ -1,6 +1,10 @@
 import AifiToken from "@/components/aifiToken";
+import Artidevelopment from "@/components/artidevelopment";
 import Navbar from "@/components/navbar";
 import aifitokenimg from "@/public/images/aifitokenimg.png";
+import developmentimg from "@/public/images/developmentimg1.png";
+import developmentimg2 from "@/public/images/developmentimg2.png";
+
 import Image from "next/image";
 export default function Home() {
   return (
@@ -13,6 +17,15 @@ export default function Home() {
           <Image src={aifitokenimg} />
         </div>
         <AifiToken />
+      </main>
+      <main className="max-w-[1536px] mx-auto max-[1536px]:px-4">
+        <div className="absolute left-0">
+          <Image src={developmentimg} />
+        </div>
+        <div className="absolute right-0">
+          <Image src={developmentimg2} />
+        </div>
+        <Artidevelopment />
       </main>
     </>
   );
