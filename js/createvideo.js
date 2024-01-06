9fa05ee919ce59242beb08b2ea5a9e9852c23a84
Dev-SardarAbdul <dev.sardarabdul@gmail.com>
2024-01-06 19:39:16 +0500

    app added

--- js/createvideo.js
+++ js/createvideo.js
@@ -1,16 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   var video = document.getElementById("myVideo");
-  var playButton = document.getElementById("playButton");
-  var progressBarContainer = document.getElementById("progress-container");
-  var progressBar = document.getElementById("progress-bar");
-  var currentTimeDisplay = document.getElementById("currentTime");
-  var totalDurationDisplay = document.getElementById("totalDuration");
-  var playIcon = document.getElementById("playIcon");
-  var pauseIcon = document.getElementById("pauseIcon");
-
   var wavesurfer = WaveSurfer.create({
     container: "#audiowave",
-    // waveColor: "#53535363",
     progressColor: "#4169E1",
     height: 40,
     responsive: true,
@@ -24,52 +15,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
   wavesurfer.load("../assets/sounds/sound.mp3");
 
-  playButton.addEventListener("click", function () {
-    if (video.paused) {
-      video.play();
-      playIcon.style.display = "none";
-      pauseIcon.style.display = "block";
-      wavesurfer.playPause();
-    } else {
-      video.pause();
-      playIcon.style.display = "block";
-      pauseIcon.style.display = "none";
-      wavesurfer.playPause();
-    }
+  video.addEventListener("play", function () {
+    wavesurfer.play();
   });
 
-  video.addEventListener("timeupdate", function () {
-    // Calculate progress as a percentage
-    var progress = (video.currentTime / video.duration) * 100;
-    progressBar.style.width = progress + "%";
-
-    // Update current time display
-    currentTimeDisplay.innerText = formatTime(video.currentTime);
-
-    // Update total duration display
-    totalDurationDisplay.innerText = formatTime(video.duration);
+  video.addEventListener("pause", function () {
+    wavesurfer.pause();
   });
 
-  progressBarContainer.addEventListener("click", function (event) {
-    // Calculate the click position relative to the progress bar container
-    var clickX =
-      event.clientX - progressBarContainer.getBoundingClientRect().left;
-    var progressBarWidth = progressBarContainer.offsetWidth;
-
-    // Calculate the percentage of the click position
-    var clickPercentage = (clickX / progressBarWidth) * 100;
-
-    // Set the new time in the video based on the click percentage
-    var newTime = (clickPercentage / 100) * video.duration;
-    video.currentTime = newTime;
+  video.addEventListener("ended", function () {
+    wavesurfer.stop();
   });
-
-  // Helper function to format time in MM:SS format
-  function formatTime(time) {
-    var minutes = Math.floor(time / 60);
-    var seconds = Math.floor(time % 60);
-    return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
-  }
 });
 
 const imageContainers = document.querySelectorAll(".image-container");
